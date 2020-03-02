e2e32b587f859574f128eff683bfe90ecf3d1aeb
angelfullstack <angelfullstack@gmail.com>
2020-03-02 20:57:22 +0100

    routing raíz a Home  component. Anidado listCoversC en HomeC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/' },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
