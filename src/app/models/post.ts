46e861a92a03fd6e8874e96dde5a19a9251b59a2
angelfullstack <angelfullstack@gmail.com>
2020-03-02 20:35:20 +0100

    generado Post model

---
+++ src/app/models/post.ts
@@ -0,0 +1,29 @@
+export class Post {
+  id: number;
+  title: string;
+  image: string;
+  content: string;
+  category: string;
+  autor: string;
+  autors: number;
+  likes: number;
+  constructor(
+    pId: number,
+    pTitle: string,
+    pImage: string,
+    pContent: string,
+    pCategory: string,
+    pAutor: string,
+    pAutors:number,
+    pLikes: number,
+  ) {
+    this.id = pId;
+    this.title = pTitle;
+    this.image = pImage;
+    this.content = pContent;
+    this.category = pCategory;
+    this.autor = pAutor;
+    this.autors = pAutors;
+    this.likes = pLikes;
+  }
+}
