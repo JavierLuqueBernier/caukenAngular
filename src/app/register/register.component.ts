eb0183fb01341ea1e693952b3e124ce0ab633e41
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-05 23:55:17 +0100

    Validators de register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  formularioEnviado: boolean;
+
+  constructor() { 
+    this.formularioEnviado = false;
+    this.formulario = new FormGroup({
+      email: new FormControl('', [
+        Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
+      ]),
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30)
+      ]),
+      password: new FormControl('', [
+        Validators.pattern(/^(?=.*\d).{4,8}$/)
+      ]),
+      repite_password: new FormControl()
+    }, [this.passwordValidator]);
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.formularioEnviado = true;
+    console.log(this.formulario.value);
+  }
+
+  passwordValidator(form) {
+    const passwordValue = form.controls.password.value;
+    const repitePasswordValue = form.controls.repite_password.value;
+
+    if (passwordValue === repitePasswordValue) {
+      return null;
+    } else {
+      return { passwordvalidator: true };
+    }
   }
 
 }
