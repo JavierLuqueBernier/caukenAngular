71034c6de192a37774cdb1db9245db2a7620817f
angelfullstack <angelfullstack@gmail.com>
2020-03-07 12:40:14 +0100

    Maquetación Page C terminada

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina.component.css']
 })
 export class PaginaComponent implements OnInit {
+  arrMigas: any;
 
-  constructor() { }
+  constructor() {
+    //El número de migas de pan debería ser limitado, unos 7 o menos sin scroll
+    this.arrMigas = [1, 2, 3, 4, 5, 6, 7];
+  }
 
   ngOnInit() {
   }
