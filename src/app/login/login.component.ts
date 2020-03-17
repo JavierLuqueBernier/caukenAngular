c7b47a838181498e282e3cc2f02ba9a4152fc965
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-17 13:09:19 +0100

    Relleno user.service con los métodos registro y login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+
+  formularioEnviado: boolean;
+
+  constructor() { 
+    this.formularioEnviado = false;
+    this.formLogin = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30)
+      ]),
+      password: new FormControl('', [
+        Validators.pattern(/^(?=.*\d).{4,8}$/)
+      ]),
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  baseUrl: string;
+
+  constructor(private httpClient: HttpClient) { 
+    this.baseUrl = 'http://localhost:3000/api/users';
+  }
+
+  registro(formValues) {
+    return this.httpClient.post(`${this.baseUrl}/register`, formValues).toPromise();
+  }
+
+  login(formValues) {
+    return this.httpClient.post(`${this.baseUrl}/login`, formValues).toPromise();
+  }
+  
 }
