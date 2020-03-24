7db4f9bb524c1bbe411b2bbe5a7a6522534df87a
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-24 13:53:04 +0100

    Creamos el export

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,3 +4,4 @@ export const LOGIN_ACTIVE = 'LOGIN_ACTIVE';
 export const DECISION_ACTIVE = 'DECISION_ACTIVE';
 export const ROUTE_AFTER = 'ROUTE_AFTER';
 export const POST_DATA = "POST_DATA";
+export const ID_PADRE = "ID_PADRE";
